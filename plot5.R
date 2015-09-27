468d50619b8c12e012aa737c4fc7cfcba5836daf
mollihua <mochen.mmm@gmail.com>
2015-09-27 10:30:03 -0700

    mordified plot5.R and plot5.png

--- plot5.R
+++ plot5.R
@@ -17,8 +17,9 @@ grp <- group_by(motorbalt, year)
 tot <- summarize(grp, sum(Emissions))
 result <- data.frame(year=tot[[1]],sumemi=tot[[2]])
 
-barplot(result$sumemi, main="Total Emissions from motor vehicle sources in Baltimore City"
-        , xlab="year", names.arg=c(result$year), cex.main=0.6)
+myplot <- barplot(result$sumemi, main="Total Emissions from motor sources in Baltimore"
+        , xlab="year", names.arg=c(result$year), cex.main=1.0)
 
+print (myplot)
 dev.copy(png, file="plot5.png")
 dev.off()
